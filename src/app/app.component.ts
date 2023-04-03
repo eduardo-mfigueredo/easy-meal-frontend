bfa687022689a39de7c92638f761c49bf2b22850
Eduardo Figueredo <eduardo.mfigueredo@hotmail.com>
2023-04-03 14:44:14 -0300

    injecting breakpoint-service and adjusting meal-options-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {BreakpointObserverService} from "./services/breakpoint-observer/breakpoint-observer.service";
 import {Subscription} from "rxjs";
 import {breakpoint} from "./services/breakpoints";
@@ -8,27 +8,6 @@ import {breakpoint} from "./services/breakpoints";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-
-  subscriptions: Subscription[] = [];
-  deviceType!: breakpoint | undefined;
-
-  constructor(
-    private readonly breakpointObserverService: BreakpointObserverService
-  ) {
-    this.breakpointObserverService.init();
-  }
-
-  ngOnInit(): void {
-    this.subscriptions.push(
-      this.breakpointObserverService.breakpointSize$.subscribe(device => {
-        this.deviceType = device;
-      })
-    );
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.forEach(subscription => subscription.unsubscribe());
-  }
+export class AppComponent {
 
 }
--- src/app/meal-options/meal-options.component.ts
+++ src/app/meal-options/meal-options.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {breakpoint} from "../services/breakpoints";
+import {Subscription} from "rxjs";
+import {BreakpointObserverService} from "../services/breakpoint-observer/breakpoint-observer.service";
 
 @Component({
   selector: 'app-meal-options',
@@ -10,4 +13,27 @@ export class MealOptionsComponent {
   breakfast: string = 'assets/pics/breakfast.jpg';
   lunch: string = 'assets/pics/lunch.jpg'
   treats: string = 'assets/pics/treats.jpeg'
+
+
+  subscriptions: Subscription[] = [];
+  deviceType!: breakpoint | undefined;
+
+  constructor(
+    private readonly _breakpointObserverService: BreakpointObserverService
+  ) {
+    this._breakpointObserverService.init();
+  }
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this._breakpointObserverService.breakpointSize$.subscribe(device => {
+        this.deviceType = device;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
 }
