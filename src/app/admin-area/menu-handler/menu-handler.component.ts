b5fad31cfd183e14da85b06811d10daac0dfb4f5
Eduardo Figueredo <eduardo.mfigueredo@hotmail.com>
2023-05-30 00:04:19 -0300

    wip menu handler

--- src/app/admin-area/menu-handler/menu-handler.component.ts
+++ src/app/admin-area/menu-handler/menu-handler.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FirestoreService} from "../../services/firestore/firestore.service";
+import {Observable} from "rxjs";
+import {MenuOption} from "../../integration/menu/menu-model";
 
 @Component({
   selector: 'app-menu-handler',
   templateUrl: './menu-handler.component.html',
   styleUrls: ['./menu-handler.component.scss']
 })
-export class MenuHandlerComponent {
+export class MenuHandlerComponent implements OnInit {
+
+  menuOptions$?: Observable<MenuOption[]>;
+
+  constructor(
+    private firestoreService: FirestoreService,
+  ) { }
+
+  ngOnInit(): void {
+    this.menuOptions$ = this.firestoreService.getItems('');
+  }
+
+  submit(): void {
+    this.firestoreService.addItem(
+      {
+        title: 'Teste',
+        description: 'Teste',
+        price: 10,
+      }
+    )
+  }
 
 }
